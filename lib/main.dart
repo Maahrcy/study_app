ce3435fd83c11d9c211c63f520cb22848eb3ab75
Macielly da Trindade Almeida <89543485+Maahrcy@users.noreply.github.com>
2022-08-29 11:49:44 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:study_app/home.dart';
 import 'package:flutter/material.dart';
+import 'package:study_app/login.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Home(),
+      home: Login(),
     ),
   );
 }
