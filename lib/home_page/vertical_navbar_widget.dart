edccba869cd7fb325e45f5e6cd52d55e5e7d92dd
Macielly da Trindade Almeida <89543485+Maahrcy@users.noreply.github.com>
2022-08-31 08:18:24 -0300

    Create vertical_navbar_widget.dart

---
+++ lib/home_page/vertical_navbar_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class VerticalNavBar extends StatefulWidget {
+  const VerticalNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<VerticalNavBar> createState() => _VerticalNavBarState();
+}
+
+class _VerticalNavBarState extends State<VerticalNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: 50,
+              ),
+              ListTile(
+                title: Center(
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(''),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Center(
+                  child: Text('Fulano de Tal'),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              ListTile(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black, width: 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                title: Center(
+                  child: Text('Home'),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black, width: 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                title: Center(
+                  child: Text('Material'),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black, width: 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                title: Center(
+                  child: Text('Profile'),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+    );
+  }
+}
