29b5ad22acdb7d64f4247b57bf95b906953660be
Lucas Rosendo <104103884+LucaRosendo@users.noreply.github.com>
2022-09-11 13:07:56 -0300

    Add files via upload

---
+++ BD.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../domain/materia.dart';
+import '../domain/resumo.dart';
+import '../domain/tema.dart';
+
+class BD {
+  //static Resumo resumo = Resumo(titulo: 'quaklquer', text: 'qualquer');
+  static List<Resumo> listaResumes = [
+    Resumo(
+        titulo: 'Isomeria',
+        text:
+            '  Isomeria química é um fenômeno observado quando duas ou mais substâncias orgânicas têm a mesma fórmula molecular, mas estrutura molecular e propriedades diferentes. \n '
+            '\n As substâncias químicas com essas características são denominadas isômeros. \n '
+            '\n O termo deriva das palavras gregas iso = igual e meros = partes, ou seja, partes iguais. \n '
+            '\nExistem diferentes tipos de isomeria: \n \n '
+            'Isomeria plana:'
+            'Os compostos são identificados através das fórmulas estruturais planas. Divide-se em isomeria de cadeia, isomeria de função, isomeria de posição, isomeria de compensação e isomeria de tautomeria. \n '
+            '\n Isomeria espacial: A estrutura molecular dos compostos apresenta diferentes estruturas espaciais. Divide-se em isomeria geométrica e isomeria óptica.'),
+    Resumo(
+        titulo: 'Resumo 2',
+        text: '  Lorem input não sei oq não sei oq não sei oq'),
+    Resumo(
+        titulo: 'Resumo 3',
+        text: '  Lorem input não sei oq não sei oq não sei oq'),
+    Resumo(
+        titulo: 'Resumo 4',
+        text: '  Lorem input não sei oq não sei oq não sei oq'),
+    Resumo(
+        titulo: 'Resumo 5',
+        text: '  Lorem input não sei oq não sei oq não sei oq')
+  ];
+
+  static Tema tema = Tema(
+      numtema: 1,
+      textcolor1: Colors.white,
+      textcolor2: Color(0xffd9807c),
+      textcolor3: Colors.black);
+
+  static getTema() {
+    return tema;
+  }
+
+  static List<Materia> listaMaterias = [
+    Materia(titulo: 'Matemática'),
+    Materia(titulo: 'Portugûes'),
+    Materia(titulo: 'Química'),
+    Materia(titulo: 'História'),
+    Materia(titulo: 'Geografia'),
+    Materia(titulo: 'Biologia'),
+    Materia(titulo: 'Física'),
+  ];
+
+  static lusBody(String materia) {
+    return Text(
+      materia,
+      style: const TextStyle(
+        fontSize: 20,
+        color: Colors.white,
+      ),
+    );
+  }
+}
