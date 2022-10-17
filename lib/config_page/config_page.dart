deef0245f55266d274cdb14d6163558a6810a931
jose <jfsj5@aluno.ifal.edu.br>
2022-10-17 15:10:25 -0300

    sdasdas

--- lib/config_page/config_page.dart
+++ lib/config_page/config_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:study_app/home_page/vertical_navbar_widget.dart';
 
+import '../data/BD.dart';
+
 class ConfigPage extends StatefulWidget {
   const ConfigPage({Key? key}) : super(key: key);
 
@@ -13,9 +15,10 @@ class _ConfigPageState extends State<ConfigPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black87,
       appBar: AppBar(
         title: Text('Configurações'),
-        backgroundColor: Color(0xFF8CC0DE),
+        backgroundColor: Colors.black,
       ),
       drawer: VerticalNavBar(),
       body: buildBody(context),
@@ -25,90 +28,134 @@ class _ConfigPageState extends State<ConfigPage> {
   buildBody(context) {
     return Container(
       child: Center(
-        child: ListView(
-          children: [
-            InkWell(
-              onTap: () {
-                setState(() {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context){
-                      return AlertDialog(
-                        title: new Text("Mudar o tema"),
-                        content: new Text("Futuramente, quando clicar aqui, irá mudar o tema do app."),
-                      );
-                    },
-                  );
-                });
-              },
-              child: Text('Tema'),
-            ),
-            InkWell(
-              onTap: () {
-                setState(() {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context){
-                      return AlertDialog(
-                        title: new Text("Contate-nos"),
-                        content: new Text("E-mail : example@gmail.com\nTelefone: (82) 99812 3456"),
-                      );
-                    },
-                  );
-                });
-              },
-              child: Text('Contate-nos'),
-            ),
-            InkWell(
-              onTap: () {
-                setState(() {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context){
-                      return AlertDialog(
-                        title: new Text("Sobre nós"),
-                        content: new Text("Bem, preciso consultar os outros primeiro"),
-                      );
-                    },
-                  );
-                });
-              },
-              child: Text('Sobre nós'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                    onPressed: (){
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context){
-                          return AlertDialog(
-                            title: new Text("Minus"),
-                            content: new Text("Retirando tamanho na fonte"),
-                          );
-                        },
-                      );
-                    },
-                    icon: const Icon(CupertinoIcons.minus, color: Colors.black,)),
-                Text("Fonte"),
-                IconButton(
-                  onPressed: (){
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0, top: 20.0),
+          child: ListView(
+            children: [
+              InkWell(
+                onTap: () {
+                  setState(() {
                     showDialog(
-                    context: context,
-                    builder: (BuildContext context){
-                      return AlertDialog(
-                        title: new Text("Add"),
-                        content: new Text("Adicionando tamanho na fonte"),
+                      context: context,
+                      builder: (BuildContext context){
+                        return AlertDialog(
+
+                          title: new Text("Mudar o tema",),
+                          content: new Text("Futuramente, quando clicar aqui, irá mudar o tema do app."),
                         );
                       },
                     );
-                  }, icon: const Icon(CupertinoIcons.add, color: Colors.black,)),
-              ],
-            ),
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text('Tema',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: BD.tema.textcolor1,
+                    ),),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context){
+                        return AlertDialog(
+                          title: new Text("Contate-nos"),
+                          content: new Text("E-mail : example@gmail.com\nTelefone: (82) 99812 3456"),
+                        );
+                      },
+                    );
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text('Contate-nos',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: BD.tema.textcolor1,
+                    ),),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context){
+                        return AlertDialog(
+                          title: new Text("Sobre nós"),
+                          content: new Text("Bem, preciso consultar os outros primeiro"),
+                        );
+                      },
+                    );
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text('Sobre nós',
+                    style: TextStyle(
+                    fontSize: 20,
+                      color: BD.tema.textcolor1,
+                  ),),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,//mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //Padding(
+                    //padding: const EdgeInsets.all(8.0),),
+                  Text("Fonte",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: BD.tema.textcolor1,
+                    ),),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
 
-            Center(child: Text("Ainda em construção..."),)
-          ],
+                        IconButton(
+                            onPressed: (){
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context){
+                                  return AlertDialog(
+                                    title: new Text("Minus"),
+                                    content: new Text("Retirando tamanho na fonte"),
+                                  );
+                                },
+                              );
+                            },
+                            icon: const Icon(CupertinoIcons.minus, color: Colors.white,)),
+                        SizedBox(width: 30.0,),
+                        IconButton(
+                          onPressed: (){
+                            showDialog(
+                            context: context,
+                            builder: (BuildContext context){
+                              return AlertDialog(
+                                title: new Text("Add"),
+                                content: new Text("Adicionando tamanho na fonte"),
+                                );
+                              },
+                            );
+                          }, icon: const Icon(CupertinoIcons.add, color: Colors.white,)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              Center(child: Text("Ainda em construção...",
+              style: TextStyle(
+                color: BD.tema.textcolor1,
+              ),),)
+            ],
+          ),
         ),
       ),
     );
