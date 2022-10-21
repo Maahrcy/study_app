eb3688d0570c91d989be08f3732e0dfbb70668fb
aluno <mta2@aluno.ifal.edu.br>
2022-10-21 11:12:58 -0300

    blabkabakd

--- lib/pages/login_page/login.dart
+++ lib/pages/login_page/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:study_app/database/user_dao.dart';
 import 'package:study_app/pages/home_page/home.dart';
 import 'package:study_app/pages/register_page/register.dart';
+import 'package:study_app/database/shared_pref_helper.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -24,106 +25,108 @@ class _LoginState extends State<Login> {
   }
 
   buildBody() {
-    return Container(
-      margin: EdgeInsets.all(20),
-      padding: EdgeInsets.all(40),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-                height: 200,
-                width: 200,
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/perfil.jpg'),
-                )),
-            const SizedBox(height: 35),
-            Align(
-                alignment: Alignment.topCenter,
-                child: Text("WELCOME!",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(fontSize: 25))),
-            const SizedBox(height: 28),
-            const SizedBox(height: 28),
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Campo de e-mail obrigatório';
-                }
-                return null;
-              },
-              controller: userController,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'User'),
-            ),
-            const SizedBox(height: 16),
-            TextFormField(
-              obscureText: true,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Campo de senha obrigatório';
-                } else if (value.length < 6) {
-                  return 'Senha deve possuir no mínimo 6 digitos';
-                }
+    return SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(40),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                    height: 200,
+                    width: 200,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/perfil.jpg'),
+                    )),
+                const SizedBox(height: 35),
+                Align(
+                    alignment: Alignment.topCenter,
+                    child: Text("WELCOME!",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(fontSize: 25))),
+                const SizedBox(height: 28),
+                const SizedBox(height: 28),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo de e-mail obrigatório';
+                    }
+                    return null;
+                  },
+                  controller: userController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'User'),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo de senha obrigatório';
+                    } else if (value.length < 6) {
+                      return 'Senha deve possuir no mínimo 6 digitos';
+                    }
 
-                return null;
-              },
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Password'),
-              controller: passController,
-            ),
-            const SizedBox(height: 33),
-            ElevatedButton(
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  String userInput = userController.text;
-                  String passInput = passController.text;
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Password'),
+                  controller: passController,
+                ),
+                const SizedBox(height: 33),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      String userInput = userController.text;
+                      String passInput = passController.text;
 
-                  bool result = await userDao().aut(user: userInput, password: passInput);
+                      bool result = await userDao().aut(user: userInput, password: passInput);
 
-                  if (result) {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return Home();
-                        },
-                      ),
-                    );
-                  } else {
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            content: Text('Usuário/Senha incorreto(s)'),
-                            actions: <Widget>[
-                              TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: Text('OK'))
-                            ],
-                          );
-                        });
-                    print('Usuário/Senha incorreto(s)');
-                  }
-                }
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Log In'),
-                ],
-              ),
+                      if (result) {
+                        await SharedPrefHelper().login();
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const Home();
+                            },
+                          ),
+                        );
+                      } else {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                content: Text('Usuário/Senha incorreto(s)'),
+                                actions: <Widget>[
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text('OK'))
+                                ],
+                              );
+                            });
+                        print('Usuário/Senha incorreto(s)');
+                      }
+                    }
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('Log In'),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: onPressedRegister,
+                    child: const Text('Register an user'))
+              ],
             ),
-            SizedBox(height: 33,),
-            ElevatedButton(
-                onPressed: onPressedRegister(),
-                child: Text('Register an user'))
-          ],
+          ),
         ),
-      ),
     );
   }
 
