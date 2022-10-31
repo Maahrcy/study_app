7d72e40216f31fbe9ebee6bb9c4939ad6f60dbb9
guilherme-gjv <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>
2022-10-31 13:28:40 -0300

    NAO TIRA O pubspec.yaml

--- lib/database/materia_dao.dart
+++ lib/database/materia_dao.dart
@@ -2,6 +2,8 @@ import 'package:study_app/database/db_helper.dart';
 import 'package:study_app/domain/materia.dart';
 import 'package:sqflite/sqflite.dart';
 
+import 'db_helper.dart';
+
 class MateriaDao {
   Future<List<Materia>> listaMaterias() async {
     DbHelper dbHelper = DbHelper();
