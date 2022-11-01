ede3a976e632cefb1f81aec278ac49f441a90868
guilherme-gjv <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>
2022-11-01 14:29:41 -0300

    fulano modificado

--- lib/pages/home_page/vertical_navbar_widget.dart
+++ lib/pages/home_page/vertical_navbar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:study_app/pages/home_page/home.dart';
 
 import '../config_page/config_page.dart';
@@ -11,95 +12,112 @@ class VerticalNavBar extends StatefulWidget {
 }
 
 class _VerticalNavBarState extends State<VerticalNavBar> {
+  String name = "Usuario";
+  Future<void> getUserName() async {
+    SharedPreferences instance = await SharedPreferences.getInstance();
+    setState(() {
+      name = instance.getString('NAME') ?? "Usuario Teste" ;
+      print('tudo certo aq');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserName();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: <Widget>[
-          const SizedBox(
-            height: 50,
-          ),
-          const ListTile(
-            title: Center(
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(''),
+    return Scaffold(
+      body: Drawer(
+        child: ListView(
+          children: <Widget>[
+            const SizedBox(
+              height: 50,
+            ),
+            const ListTile(
+              title: Center(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(''),
+                ),
               ),
             ),
-          ),
-          ListTile(
-            title: const Center(
-              child: Text('Fulano de Tal'),
+            ListTile(
+              title: Center(
+                child: Text(name),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          const SizedBox(
-            height: 100,
-          ),
-          ListTile(
-            shape: RoundedRectangleBorder(
-              side: const BorderSide(color: Colors.black, width: 1),
-              borderRadius: BorderRadius.circular(5),
+            const SizedBox(
+              height: 100,
             ),
-            title: const Center(
-              child: Text('Home'),
+            ListTile(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(color: Colors.black, width: 1),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              title: const Center(
+                child: Text('Home'),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const Home();
+                    },
+                  ),
+                );
+              },
             ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const Home();
-                  },
-                ),
-              );
-            },
-          ),
-          ListTile(
-            shape: RoundedRectangleBorder(
-              side: const BorderSide(color: Colors.black, width: 1),
-              borderRadius: BorderRadius.circular(5),
+            ListTile(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(color: Colors.black, width: 1),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              title: const Center(
+                child: Text('Material'),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
-            title: const Center(
-              child: Text('Material'),
+            ListTile(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(color: Colors.black, width: 1),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              title: const Center(
+                child: Text('Profile'),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            shape: RoundedRectangleBorder(
-              side: const BorderSide(color: Colors.black, width: 1),
-              borderRadius: BorderRadius.circular(5),
+            ListTile(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(color: Colors.black, width: 1),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              title: const Center(
+                child: Text('Configurações'),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const ConfigPage();
+                    },
+                  ),
+                );
+              },
             ),
-            title: const Center(
-              child: Text('Profile'),
-            ),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            shape: RoundedRectangleBorder(
-              side: const BorderSide(color: Colors.black, width: 1),
-              borderRadius: BorderRadius.circular(5),
-            ),
-            title: const Center(
-              child: Text('Configurações'),
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const ConfigPage();
-                  },
-                ),
-              );
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
